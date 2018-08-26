e8ccaeadb079bad8f4b09fda1770809fb4ddcc2e
James Kane <jameskane05@gmail.com>
2018-08-26 14:47:26 -0500

    Initial commit

---
+++ Assets/Scripts/CornerCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CornerCollision : MonoBehaviour
+{
+
+    public int cornerNum;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ExperimentSettings _expInstance = ExperimentSettings.GetInstance();
+        if (ExperimentSettings.IsTSC())
+        {
+            Debug.Log(gameObject.transform.position);
+            MazeController.cornerTransform = gameObject.transform;
+            gameObject.SetActive(false);
+            //Debug.Log(gameObject.ToString());
+            MazeController.onCorner = cornerNum;
+            GameObject landmarkList = GameObject.Find("LandmarkList");
+            Transform landmarkNameTransform = landmarkList.transform.GetChild(cornerNum);
+            if (cornerNum > 0)
+            {
+                Transform lastLandmarkNameTransform = landmarkList.transform.GetChild(cornerNum - 1);
+                Text lastLandmarkNameText = lastLandmarkNameTransform.GetComponent<Text>();
+                lastLandmarkNameText.fontSize = 14;
+                lastLandmarkNameText.fontStyle = FontStyle.Normal;
+            }
+            Text landmarkNameText = landmarkNameTransform.GetComponent<Text>();
+            landmarkNameText.fontSize = 20;
+            landmarkNameText.fontStyle = FontStyle.Bold;
+
+        }
+
+    }
+
+
+}
---
+++ Assets/_Imports/DoorKits/Door7/Script/Door_Script_Door7.js
@@ -0,0 +1,46 @@
+﻿// Smothly open a door
+var DoorSpeed = 2.0;
+var DoorOpenAngle = 90.0;
+var DoorSound:AudioClip;
+private var open : boolean;
+private var enter : boolean;
+
+private var defaultRot : Vector3;
+private var openRot : Vector3;
+
+function Start(){
+defaultRot = transform.eulerAngles;
+openRot = new Vector3 (defaultRot.x, defaultRot.y +- DoorOpenAngle, defaultRot.z);
+}
+
+//Main function
+
+function Update (){
+if(open){
+//Open door
+transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, Time.deltaTime * DoorSpeed);
+}
+
+if(Input.GetKeyDown("f") && enter){
+open = !open;
+AudioSource.PlayClipAtPoint(DoorSound, transform.position);
+         {
+             GetComponent.<Animation>().Play("door_trigger");
+         };
+  }
+}
+
+//Activate the Main function when player is near the door
+function OnTriggerEnter (other : Collider){
+if (other.gameObject.tag == "Player") {
+enter = true;
+}
+}
+
+//Deactivate the Main function when player is go away from door
+function OnTriggerExit (other : Collider){
+if (other.gameObject.tag == "Player") {
+enter = false;  
+}
+}
+
